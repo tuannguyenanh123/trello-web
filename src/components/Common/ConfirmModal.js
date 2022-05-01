d01878a0edb4b750afcae61235a8d25ed2777676
tuannguyenanh123 <anhtuan120600ohayo@gmail.com>
2022-05-01 09:55:07 +0700

    update and remove column

---
+++ src/components/Common/ConfirmModal.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+import HTMLReactParser from "html-react-parser";
+
+import {
+  MODAL_ACTION_CONFIRM,
+  MODAL_ACTION_CLOSE,
+} from "./../../utilities/constants";
+
+const ConfirmModal = ({ show, title, onAction, content }) => {
+  return (
+    <Modal
+      show={show}
+      onHide={() => {
+        onAction(MODAL_ACTION_CLOSE);
+      }}
+      backdrop="static"
+      keyboard={false}
+      animation={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className="h5">{HTMLReactParser(title)}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{HTMLReactParser(content)}</Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant="secondary"
+          onClick={() => onAction(MODAL_ACTION_CLOSE)}
+        >
+          Close
+        </Button>
+        <Button
+          variant="primary"
+          onClick={() => onAction(MODAL_ACTION_CONFIRM)}
+        >
+          Confirm
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ConfirmModal;
